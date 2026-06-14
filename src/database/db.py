0e287076556355ea4601faa8727680e8918cca26
Ayush Porwal <ayushporwal2006@gmail.com>
2026-06-14 18:59:10 +0530

    Update db.py

--- src/database/db.py
+++ src/database/db.py
@@ -82,5 +82,4 @@ def create_attendance(logs):
 
 def get_attendance_for_teacher(teacher_id):
     response = supabase.table("attendance_logs").select("*, subjects!inner(*)").eq('subjects.teacher_id', teacher_id).execute()
-    print(response.data)
     return response.data
