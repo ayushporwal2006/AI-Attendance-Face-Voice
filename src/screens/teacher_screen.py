4eef3c4475eebfd8653aa20c284dcd450173cb88
Ayush Porwal <ayushporwal2006@gmail.com>
2026-06-24 02:05:37 +0530

    Update teacher_screen.py

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -211,7 +211,7 @@ def teacher_tab_attendance_record():
             "ts_group": ts.split(".")[0] if ts else None,
             "Time": datetime.fromisoformat(ts).strftime("%Y-%m-%d %I:%M:%p") if ts else "N'A",
             "Subject":r["subjects"]['name'],
-            "Subject code":r['subjects']['subject_code'],
+            "Subject Code":r['subjects']['subject_code'],
             "is_present": bool(r.get('is_present', False))
         })
 
